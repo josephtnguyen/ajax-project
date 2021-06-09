677bec5fded76e328b4d74fd3d31b5ab86fbda78
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-06-09 09:52:51 -0700

    Load in numbers for each day of the month, starting on the current day.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
 /* exported data */
+var data = {
+  home: null,
+  days: []
+};
+
+var previousData = localStorage.getItem('calendar-data');
+if (previousData) {
+  data = JSON.parse(previousData);
+}
+
+window.addEventListener('beforeunload', handleSave);
+
+function handleSave(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('calendar-data', dataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,161 @@
+var today = {
+  day: new Date().getDate(),
+  month: new Date().getMonth(),
+  year: new Date().getFullYear()
+};
+
+var view = {
+  day: today.day,
+  month: today.month,
+  year: today.year
+};
+
+var $calendarMonthYear = document.querySelector('.header-month-year');
 var $calendar = document.querySelector('.calendar-squares');
+var $previousMonth = document.querySelector('.left-arrow-button');
+var $nextMonth = document.querySelector('.right-arrow-button');
+
+$previousMonth.addEventListener('click', handlePrevious);
+$nextMonth.addEventListener('click', handleNext);
 
 generateSquares($calendar);
+populateCalendar(today.month, today.year);
 
+// Event Handlers
+function handlePrevious(event) {
+  if (view.month === 0) {
+    view.month = 11;
+    view.year--;
+  } else {
+    view.month--;
+  }
+  generateSquares($calendar);
+  populateCalendar(view.month, view.year);
+}
+
+function handleNext(event) {
+  if (view.month === 11) {
+    view.month = 0;
+    view.year++;
+  } else {
+    view.month++;
+  }
+  generateSquares($calendar);
+  populateCalendar(view.month, view.year);
+}
+
+// General Functions
 function generateSquares(calendar) {
+  calendar.innerHTML = '';
   for (var i = 0; i < 6; i++) {
     var $row = document.createElement('div');
     $row.className = 'row';
     for (var j = 0; j < 7; j++) {
       var $square = document.createElement('div');
       $square.className = 'col-14 square';
+      $square.setAttribute('id', i * 10 + j);
       $row.append($square);
     }
     calendar.append($row);
   }
 }
+
+function generateHTMLCalendarDay(square, day, isCurrentMonth) {
+  var $heading = document.createElement('div');
+  $heading.className = 'row';
+
+  // Number
+  var $numberDiv = document.createElement('div');
+  $numberDiv.className = 'col-33-lg middle';
+
+  var $number = document.createElement('button');
+  $number.className = 'date';
+  if (isCurrentMonth) {
+    $number.classList.add('black');
+  } else {
+    $number.classList.add('light-gray');
+  }
+  $number.textContent = day;
+
+  // Location/Weather
+  var $headingSideDiv = document.createElement('div');
+  $headingSideDiv.className = 'col-66-lg mobile-hidden date-heading';
+
+  // Events
+  var $body = document.createElement('div');
+  $body.className = 'row mobile-hidden';
+
+  var $listDiv = document.createElement('div');
+  $listDiv.className = 'col-100';
+
+  var $list = document.createElement('ul');
+  $list.className = 'events';
+
+  square.append($heading);
+  square.append($body);
+  $heading.append($numberDiv);
+  $heading.append($headingSideDiv);
+  $numberDiv.append($number);
+  $body.append($listDiv);
+  $listDiv.append($list);
+
+}
+
+function populateCalendar(month, year) {
+  // update header
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  $calendarMonthYear.children[0].textContent = months[month];
+  $calendarMonthYear.children[1].textContent = year;
+
+  // fill in calendar
+  var thisMonth = new Date(year, month, 1);
+  // wind back to Sunday
+  var firstDay = thisMonth.getDay();
+  for (var dayOfWeek = firstDay; dayOfWeek > 0; dayOfWeek--) {
+    thisMonth.setDate(thisMonth.getDate() - 1);
+  }
+
+  // loop through all squares
+  for (var i = 0; i < $calendar.children.length; i++) {
+    for (var j = 0; j < 7; j++) {
+      var $square = $calendar.children[i].children[j];
+      var currentDay = thisMonth.getDate();
+      var isCurrentMonth = true;
+      if (thisMonth.getMonth() !== month) {
+        isCurrentMonth = false;
+      }
+      generateHTMLCalendarDay($square, currentDay, isCurrentMonth);
+      thisMonth.setDate(currentDay + 1);
+    }
+  }
+
+}
+
+// function populateCalendarDay(dayObject) {
+// }
+
+// // OOP Objects
+// function CalendarDay() {
+//   this.date = null;
+
+//   this.weather = null;
+
+//   this.holiday = null;
+//   this.travel = null;
+
+//   this.birthdays = [];
+//   this.meetings = [];
+//   this.hangouts = [];
+//   this.events = [];
+// }
+
+// function CalendarEvent(type, input, time = null) {
+//   this.type = type;
+//   this.input = input;
+//   if (time) {
+//     this.time = time;
+//   }
+//   if (type === 'birthday') {
+//     this.input = input.capitalize() + "'s Birthday";
+//   }
+// }
